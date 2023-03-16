a13f1315c9805dbb5688d6f31fc57a0787e9d20d
Jin_Pajama <alpha99kr@gmail.com>
2023-03-16 15:21:06 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -11,6 +11,12 @@ function login(){
         id : id.value,
         pw : pw.value,
     };
-    console.log('asdf'); 
-    console.log(req);   
+
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req),
+    });
 }
